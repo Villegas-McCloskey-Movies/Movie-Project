9ec1771129c90b7602aff816802486c18324adc0
Ralph <mccloskey.ralph35@gmail.com>
2022-12-08 17:56:25 -0500

    continued building add-movie form

--- js/movie-project.js
+++ js/movie-project.js
@@ -6,16 +6,19 @@
     // fetch("https://vast-organic-farm.glitch.me/movies/3").then(resp => resp.json()).then(data => console.log(data)); // get one
 
     // HOW TO POST
-    // const newMovie = {
-    //     title: "Dr. Dolittle"
-    // };
-    // fetch("https://vast-organic-farm.glitch.me/movies", {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(newMovie),
-    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+    let newMovie = {document
+    const newMovie = {
+
+        title: ${#add-movie-title},
+        rating: ${#add-rating},
+    };
+    fetch("https://field-mini-lyric.glitch.me/movies", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    }).then(() => fetch("https://field-mini-lyric.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
 
     // HOW TO UPDATE
     // const complexMovie = {
@@ -29,26 +32,26 @@
     //     title: "The Never-Ending Story"
     // }
 
-    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    // fetch("https://field-mini-lyric.glitch.me/movies/7", {
     //     method: "PUT",
     //     headers: {
     //         'Content-Type': 'application/json'
     //     },
     //     body: JSON.stringify(edittedMovie)
-    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+    // }).then(() => fetch("https://field-mini-lyric.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
 
-    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    // fetch("https://field-mini-lyric.glitch.me/movies/7", {
     //     method: "PATCH",
     //     headers: {
     //         'Content-Type': 'application/json'
     //     },
     //     body: JSON.stringify(edittedMovie)
-    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+    // }).then(() => fetch("https://field-mini-lyric.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
 
     // HOW TO DELETE
-    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    // fetch("https://field-mini-lyric.glitch.me/movies/7", {
     //     method: "DELETE"
-    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+    // }).then(() => fetch("https://field-mini-lyric.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
 
 
 })();
