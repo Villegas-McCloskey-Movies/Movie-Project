79b5c809cd08fe3d0b9e2fad259fd64df59eb2ec
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-08 16:39:31 -0600

    movie-project initial setup

---
+++ js/movie-project.js
@@ -0,0 +1,54 @@
+"use strict";
+
+(()=>{
+// HOW TO GET
+    fetch("https://field-mini-lyric.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data)); // get all
+    // fetch("https://vast-organic-farm.glitch.me/movies/3").then(resp => resp.json()).then(data => console.log(data)); // get one
+
+    // HOW TO POST
+    // const newMovie = {
+    //     title: "Dr. Dolittle"
+    // };
+    // fetch("https://vast-organic-farm.glitch.me/movies", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(newMovie),
+    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+    // HOW TO UPDATE
+    // const complexMovie = {
+    //     title: "Be Kind Rewind",
+    //     director: "Michael Gondry",
+    //     release_date: 2008,
+    //     cast: ["Yasiin Bey", "Jack Black", "Danny Glover"]
+    // };
+    //
+    // const edittedMovie = {
+    //     title: "The Never-Ending Story"
+    // }
+
+    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    //     method: "PUT",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(edittedMovie)
+    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    //     method: "PATCH",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(edittedMovie)
+    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+    // HOW TO DELETE
+    // fetch("https://vast-organic-farm.glitch.me/movies/7", {
+    //     method: "DELETE"
+    // }).then(() => fetch("https://vast-organic-farm.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+
+})();
