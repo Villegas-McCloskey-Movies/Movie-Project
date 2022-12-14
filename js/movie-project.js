2903b6e710689e55b67bc040aa4ab4a3262fa168
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-14 15:38:13 -0600

    card function and clean up

--- js/movie-project.js
+++ js/movie-project.js
@@ -5,81 +5,48 @@
         $('#loading').hide();
     });
 
-
-    // sort the movies
-    var sortSelect = document.querySelector("#sort-movie-title")
-sortSelect.addEventListener('change', function sortMovies() {
-    var selectResults = [];
-    var returnedResults = [];
-    var value = sortSelect.target.value.toLowerCase()
-        fetch("https://woolly-chambray-clef.glitch.me/movies")
-            .then(resp => resp.json())
-            .then(data => {
-                if (value === "title") {
-                            data.sort(function(a, b) {
-                                return a.title.localeCompare(b.title);
-                            });
-                        } else if (value === "rating") {
-                            data.sort(function(a, b) {
-                                return a.rating - b.rating;
-                            });
-                    }
-                selectResults.push(returnedResults);
-                })
-});
-    // sortSelect.addEventListener("change", function sortMovies(movies, sortKey) => {
-    //
-    //     data.forEach (movie => {
-    //             let movieCards = '';
-    //             for (let i = 0; i < searchResults.length; i ++) {
-    //                 movieCards += '<div class="card" style="width: 18rem;">' +
-    //                     '<div class="card-body">' +
-    //                     '<h5 class="card-title">' + searchResults[i].id + '</h5>' +
-    //                     '<h5 class="card-subtitle mb-2">' + searchResults[i].title + '</h5>' +
-    //                     '<p class="card-text">' + 'Rating: ' + searchResults[i].rating + '</p>' +
-    //                     '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + searchResults[i].id +'"> Edit </button>' +
-    //                     '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + searchResults[i].id +'"> Delete </button>' +
-    //                     ' </div>' +
-    //                     '</div>'
-    //             }
-    //             $('#movie-cards').html(movieCards);
-    //         });
-    // })
-    // function sortMovies(movies, sortKey) {
-    //
-    //     }
-    // }
-
     var fetchAllURL = "https://woolly-chambray-clef.glitch.me/movies"
 
-// HOW TO GET
-    fetch("https://woolly-chambray-clef.glitch.me/movies")
-        .then(resp => resp.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error)); // get all
-
+    //// Load Movie Cards Function ////
     function loadMovies(movies) {
         return fetch(fetchAllURL)
             .then(resp => resp.json().then(function(data) {
-                var movies = data;
-                let movieCards = '';
-                for (let i = 0; i < movies.length; i ++) {
-                    movieCards += '<div class="card" style="width: 18rem;">' +
-                        '<div class="card-body">' +
-                        '<h5 class="card-title">' + movies[i].id + '</h5>' +
-                        '<h5 class="card-subtitle mb-2">' + movies[i].title + '</h5>' +
-                        '<p class="card-text">' + 'Rating: ' + movies[i].rating + '</p>' +
-                        '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + movies[i].id +'"> Edit </button>' +
-                        '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + movies[i].id +'"> Delete </button>' +
-                        ' </div>' +
-                        '</div>'
-                }
-                $('#movie-cards').html(movieCards);
+                // let movieCards = '';
+                // for (let i = 0; i < movies.length; i ++) {
+                //     movieCards += '<div class="card" style="width: 18rem;">' +
+                //         '<div class="card-body">' +
+                //         '<h5 class="card-title">' + movies[i].id + '</h5>' +
+                //         '<h5 class="card-subtitle mb-2">' + movies[i].title + '</h5>' +
+                //         '<p class="card-text">' + 'Rating: ' + movies[i].rating + '</p>' +
+                //         '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + movies[i].id +'"> Edit </button>' +
+                //         '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + movies[i].id +'"> Delete </button>' +
+                //         ' </div>' +
+                //         '</div>'
+                // }
+                // $('#movie-cards').html(movieCards);
+                renderMovieCard(data)
             }))
             .catch(error => console.error(error));
     }
     loadMovies();
-    //  Search Movies
+
+    function renderMovieCard(data){
+        let movieCards = '';
+        for (let i = 0; i < data.length; i ++) {
+                movieCards += '<div class="card" style="width: 18rem;">' +
+                '<div class="card-body">' +
+                '<h5 class="card-title">' + data[i].id + '</h5>' +
+                '<h5 class="card-subtitle mb-2">' + data[i].title + '</h5>' +
+                '<p class="card-text">' + 'Rating: ' + data[i].rating + '</p>' +
+                '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + data[i].id +'"> Edit </button>' +
+                '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + data[i].id +'"> Delete </button>' +
+                ' </div>' +
+                '</div>'
+        }
+        $('#movie-cards').html(movieCards);
+    }
+
+    //// Search Movies ////
     var searchInput = document.querySelector("[data-search]")
 
     searchInput.addEventListener("keyup", searchInput => {
@@ -93,19 +60,20 @@ sortSelect.addEventListener('change', function sortMovies() {
                 searchResults.push(movie);
             }
         })
-                let movieCards = '';
-                for (let i = 0; i < searchResults.length; i ++) {
-                    movieCards += '<div class="card" style="width: 18rem;">' +
-                        '<div class="card-body">' +
-                        '<h5 class="card-title">' + searchResults[i].id + '</h5>' +
-                        '<h5 class="card-subtitle mb-2">' + searchResults[i].title + '</h5>' +
-                        '<p class="card-text">' + 'Rating: ' + searchResults[i].rating + '</p>' +
-                        '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + searchResults[i].id +'"> Edit </button>' +
-                        '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + searchResults[i].id +'"> Delete </button>' +
-                        ' </div>' +
-                        '</div>'
-                }
-                $('#movie-cards').html(movieCards);
+                // let movieCards = '';
+                // for (let i = 0; i < searchResults.length; i ++) {
+                //     movieCards += '<div class="card" style="width: 18rem;">' +
+                //         '<div class="card-body">' +
+                //         '<h5 class="card-title">' + searchResults[i].id + '</h5>' +
+                //         '<h5 class="card-subtitle mb-2">' + searchResults[i].title + '</h5>' +
+                //         '<p class="card-text">' + 'Rating: ' + searchResults[i].rating + '</p>' +
+                //         '<button id="edit-button" class="edit-button btn btn-primary" data-id="' + searchResults[i].id +'"> Edit </button>' +
+                //         '<button id="deleteB" class="deleteButton btn btn-primary" data-id="' + searchResults[i].id +'"> Delete </button>' +
+                //         ' </div>' +
+                //         '</div>'
+                // }
+                // $('#movie-cards').html(movieCards);
+                renderMovieCard(searchResults)
             });
     })
 
@@ -114,7 +82,7 @@ sortSelect.addEventListener('change', function sortMovies() {
         $("#edit-movie").toggleClass("hidden");
     });
 
-        ////Add Movie Form////
+    ////Add Movie Form////
     let addMovieForm = document.querySelector('#add-movie');
     addMovieForm.addEventListener("submit", (e) =>{
         e.preventDefault();
